0999c996f409df92e3a92ff65c45c1ab9c959dd8
unknown <vova_mohov@bk.ru>
2022-09-22 14:45:52 +0300

    added uprostika

--- uprostika.py
+++ uprostika.py
@@ -5,7 +5,11 @@ mat1 = [[int(i) for i in input().split()] for _ in range(n1)]
 def uproshenie2(n, m, mat):
     flag = True
     for i in mat:
-        for delitel in range(max(i)+1, 1, -1):
+        k = min(i)
+        if k == 0:
+            k = max(i)
+
+        for delitel in range(k, 1, -1):
             for j in range(m):
                 if i[j] % delitel != 0:
                     flag = False
